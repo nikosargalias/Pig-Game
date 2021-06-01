4c19a54005cad201d89e71c820f69b48afc660ab
Nikolaos P Argalias <nikosgalias@gmail.com>
2021-06-01 16:15:24 +0100

    added config

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "js", "index.js") },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      filename: "index.html",
+      // publicPath: path.resolve(__dirname, 'dist'),
+      // excludeChunks: ['entryPoint']
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  // resolve: {
+  //   // ... rest of the resolve config
+  //   fallback: {
+  //     path: require.resolve("path-browserify"),
+  //     util: require.resolve("util/"),
+  //     stream: require.resolve("stream-browserify"),
+  //   },
+  // },
+};
